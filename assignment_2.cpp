293187f40dad818c2fa11b35b7430704bb92bdf1
msterkel <msterkel@mail.csuchico.edu>
2018-02-21 13:43:06 -0800

    update action messages

--- assignment_2.cpp
+++ assignment_2.cpp
@@ -29,6 +29,9 @@ int main() {
   bool user_dead, computer_dead, draw;
   int user_gun;
   int computer_gun;
+  int victory = 0;
+  int defeat = 0;
+  int tie =0;
   
   // Seed the Random Number Generator
   srand(time(0));
@@ -47,21 +50,31 @@ ClearScreen();
   computer_gun = 0;
 
   do {
-    if(user_gun == 0) {
-  cout << "                         Gun: Unloaded" << endl;
+    if (user_gun == 0 && computer_gun == 0) {
+     cout << "Ememy Gun: Unloaded                       Your Gun: Unloaded" <<
+      endl<<endl;
 }
-else { cout << "                       Gun: Loaded " << endl;
+else if (user_gun == 0 && computer_gun != 0) {
+ cout << " Enemy Gun: Loaded                     Your Gun: Unloaded " <<
+  endl<<endl;
+}
+else if (user_gun != 0 && computer_gun == 0) {
+ cout << " Enemy Gun: Unloaded                     Your Gun: Loaded " << 
+  endl<<endl;
+}
+else { cout << " Enemy Gun: Loaded                     Your Gun: Loaded " <<
+ endl<<endl;
 }
 
-cout << "Make your move! \n\"r\" for reload - "<< endl;
-cout << "\"s\" for shoot (if  gun is loaded) - "  << endl;
-cout << "\"b\" for block" << endl;
+  cout << "Make your move! \n\"r\" for reload - "<< endl;
+  cout << "\"s\" for shoot (if gun is loaded) - "  << endl;
+  cout << "\"b\" for block" << endl;
 
 if(user_gun == 0) {
 player_choice = tolower(reader.readChar("BbRr"));
 }
 else {
-player_choice = tolower(reader.readChar("BbSsRr"));
+player_choice = tolower(reader.readChar("BbSs"));
 }
 
 ClearScreen();
@@ -69,7 +82,8 @@ ClearScreen();
 if(computer_gun == 0) {
   computer_choice = rand() % 2;
 }
-else{computer_choice = rand() % 2 +1;
+else {
+  computer_choice = rand() % 2 +1;
 }
 
 if(player_choice == 'r' && computer_choice == 0) { 
@@ -83,15 +97,15 @@ if(player_choice == 'r' && computer_choice == 0) {
   }
   else if(player_choice == 'r' && computer_choice == 2) {
     user_dead = true;
-    cout << "You've been hit! \nGame over pal." << endl;
+    cout << "You've been hit! \nGame over pal.\n" << endl;
   }
   
   else if(player_choice == 'b' && computer_choice == 0) {
     computer_gun = 1;
-    cout << "Successful evade!" << endl;
+    cout << "Carefull, your opponent's gun is now loaded!" << endl;
   }
   else if(player_choice == 'b' && computer_choice == 1) {
-     cout << "Successful evade!" << endl;
+     cout << "Looks like you both blocked...cowards!" << endl;
   }
   else if(player_choice == 'b' && computer_choice == 2) {
       computer_gun = 0;
@@ -99,7 +113,7 @@ if(player_choice == 'r' && computer_choice == 0) {
   }
   else if(player_choice == 's' && computer_choice == 0){
     computer_dead = true;
-    cout << "You got em'!" << endl;
+    cout << "You got em'!\n" << endl;
   }
   else if(player_choice == 's' && computer_choice == 1) {
     cout << "Ah shucks...your opponent evaded your shot." << endl;
@@ -107,7 +121,7 @@ if(player_choice == 'r' && computer_choice == 0) {
   }
   else if(player_choice == 's' && computer_choice == 2) {
     draw = true;
-    cout << "You got each other... a draw I reckon." << endl;
+    cout << "You got each other... a draw I reckon.\n" << endl;
   }
   
   else{ cout << "Game Has Crashed - Sorry Partner" << endl; }
@@ -121,8 +135,16 @@ computer_gun  <<endl;
 */
 
 } while(user_dead == false && computer_dead == false &&  draw == false); 
-// do while loop vs while loop ?
 
+  if(user_dead == true) {
+   ++defeat;}
+  else if(computer_dead == true) {
+    ++victory;}
+  else {
+    ++tie;}
+
+  cout << "Wins: " << victory << " " << "Losses: " << defeat << " " << "Ties: " 
+       << tie << endl;   
 
     // Prompt user to see if the want to continue to play
     cout << "Would you like to play again? (Y / N)\n";
